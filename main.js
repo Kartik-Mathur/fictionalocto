5fa06878ab79696c336adc85bac1b2a86d80c59f
raja3323 <rajakumar332323@gmail.com>
2025-05-10 15:25:00 +0530

    Now, selection sort can be used.

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const bubbleSort = require('./bubbleSort')
-
+const selectionSort = require('./selectionSort')
 console.log('hello world');
 
 let numbers = [64, 25, 12, 22, 11];
 console.log("Sorted array:", bubbleSort(numbers));
 
-
+let numbers2 = [64, 25, 12, 22, 11];
+console.log("Sorted array:", selectionSort(numbers));
 // added a comment
