ed130f441a8060fbdfe21fa9e8c4eafb41688ad3
raja3323 <rajakumar332323@gmail.com>
2025-05-10 14:44:44 +0530

    Adding bubbleSort code

---
+++ bubbleSort.js
@@ -0,0 +1,25 @@
+function bubbleSort(arr) {
+  let n = arr.length;
+  let swapped;
+
+  for (let i = 0; i < n - 1; i++) {
+    swapped = false;
+
+    for (let j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        // Swap elements
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        swapped = true;
+      }
+    }
+
+    // If no elements were swapped, array is already sorted
+    if (!swapped) {
+      break;
+    }
+  }
+
+  return arr;
+}
